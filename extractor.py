289312cc98d030631e7956018ec16dd6c37de22a
Anton Drozdov <anton_drozdov@yahoo.co.uk>
2017-04-20 14:33:32 +0100

    add: get_best_prices_for_market() to extractor.py

--- extractor.py
+++ extractor.py
@@ -31,3 +31,29 @@ def get_markets_from_oddschecker(url):
 			list_of_markets.append(market_name.group(1))
 	
 	return list_of_markets
+
+
+
+def get_best_prices_for_market(url)
+
+	"""
+	Given URL, returns dictionary with selections and prices for correcponding fixture and market
+	"""
+
+	bs4 = from_url_to_bs4(url)
+	best_odds = bs4.findAll(class_ = "add-to-bet-basket")
+	selections = []
+	prices = []
+	prices_dict = {}
+
+	for record in best_odds:
+		selection = record['data-name']    
+		price_string = record['data-ng-click']
+		price = re.search("(.*), (.*), (.*)\)", price_string).group(3)
+		selections.append(selection)
+		prices.append(price) 
+
+	prices_dict["selection"] = selections
+	prices_dict["price"] = prices
+	
+	return prices_dict
