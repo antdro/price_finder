8e6a23e6c5b86df9f0ed844a43867a1f66552a03
Anton Drozdov <anton_drozdov@yahoo.co.uk>
2017-04-24 23:40:09 +0100

    add: remove_duplicate_prices() to helper.py

--- helper.py
+++ helper.py
@@ -1,6 +1,7 @@
 # Python 3.6.0 |Anaconda 4.3.1 (64-bit)|
 
 import datetime
+import pandas as pd
 
 def get_current_date():
 	
@@ -12,3 +13,15 @@ def get_current_date():
 	date = now.strftime("%Y-%m-%d %H:%M")
 	
 	return date
+
+
+
+def remove_duplicate_prices(df):
+	
+	"""
+	Remove duplicate prices from dataframe
+	"""
+	
+	df.drop_duplicates(subset = ['fixture', 'kickoff', 'market', 'selection', 'price'], inplace = True)
+	
+	return df
