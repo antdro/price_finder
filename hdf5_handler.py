9573c87adc8a632f1b9681e1c21f536c24ffe97f
Anton Drozdov <anton_drozdov@yahoo.co.uk>
2017-04-24 20:11:44 +0100

    add: print_h5_structure() to hdf5_handler.py

--- hdf5_handler.py
+++ hdf5_handler.py
@@ -16,3 +16,22 @@ def create_football_h5(leagues):
             football.create_group(country + '/' + league)
             
     return file
+
+
+
+def print_h5_structure(file):
+    
+    """
+    Prints the structure of given hdf5 file
+    """
+    
+    for sport in file.keys():
+        print (sport)
+        for country in file[sport]:
+            print ('\t' + country)
+            for league in file[sport][country]:
+                print ('\t\t' + league)
+                for table in file[sport][country][league]:
+                    print ('\t\t\t' + table)
+        print ('\n')
+
