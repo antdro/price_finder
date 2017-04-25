f0b67a4f387d7b25ac7a0d35bf6e1871233989ec
Anton Drozdov <anton_drozdov@yahoo.co.uk>
2017-04-25 19:15:32 +0100

    add: add_league_to_football_h5() in hdf5_handler.py

--- hdf5_handler.py
+++ hdf5_handler.py
@@ -29,3 +29,23 @@ def print_h5_structure(file):
                 for table in file[sport][country][league]:
                     print ('\t\t\t' + table)
         print ('\n')
+
+        
+        
+def add_league_to_football_h5(leagues):    
+    
+    """
+    Adds leagues from {country: league} dict to football h5 file; returns updated h5
+    """
+    
+    h5 = h5py.File('oddschecker.h5', 'r+')
+    
+    football = h5['football']
+    
+    for country in leagues.keys():
+        for league in leagues[country]:
+            football.create_group(country + '/' + league)
+    
+    h5.close()
+    
+    return h5
